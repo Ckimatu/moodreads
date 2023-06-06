dfa7ed2c774ca9aae8532fbdd72d07015150f5f0
COLLINS  TONUI <tckiprotich@gmail.com>
2023-06-06 20:02:28 +0000

    Landing Page

--- newclient/src/components/App.vue
+++ newclient/src/components/App.vue
@@ -3,7 +3,7 @@
 
     <router-view />
     
-    <Footer />
+    <!-- <Footer /> -->
     
 </template>
 
--- newclient/src/components/pages/LandingPage.vue
+++ newclient/src/components/pages/LandingPage.vue
@@ -1,46 +1,100 @@
 <template>
-    <div class="bg-black h-screen w-screen">
-      
-  
-      <div class="relative isolate px-6 pt-4 lg:px-8">
-        <div class="absolute inset-x-0 -top-30 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
-          <div class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" />
-        </div>
-        <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-          <div class="hidden sm:mb-8 sm:flex sm:justify-center">
-            
+  <div class="bg-black h-screen w-screen">
+    <div class="relative isolate px-6 pt-4 lg:px-8">
+      <div
+        class="absolute inset-x-0 -top-30 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+        aria-hidden="true"
+      >
+        <div
+          class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+          style="
+            clip-path: polygon(
+              74.1% 44.1%,
+              100% 61.6%,
+              97.5% 26.9%,
+              85.5% 0.1%,
+              80.7% 2%,
+              72.5% 32.5%,
+              60.2% 62.4%,
+              52.4% 68.1%,
+              47.5% 58.3%,
+              45.2% 34.5%,
+              27.5% 76.7%,
+              0.1% 64.9%,
+              17.9% 100%,
+              27.6% 76.8%,
+              76.1% 97.7%,
+              74.1% 44.1%
+            );
+          "
+        />
+      </div>
+      <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+        <div class="hidden sm:mb-8 sm:flex sm:justify-center"></div>
+        <div class="text-center">
+          <h1
+            class="text-4xl font-bold tracking-tight text-gray-200 sm:text-6xl"
+          >
+            Find the perfect book to match your mood!
+          </h1>
+          <p class="mt-6 text-lg leading-8 text-gray-400">
+            <span className="text-blue-300 text-bold"
+              >From Classics to Hidden Treasures, </span
+            >Unleash Your Reading Potential with Tailored Book Suggestions
+            Powered by OpenAI .
+          </p>
+          <div class="mt-10 flex items-center justify-center gap-x-6">
+            <a
+              href="/home"
+              class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >Get Reccomendations</a
+            >
+            <!-- <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Learn more <span aria-hidden="true">→</span></a> -->
           </div>
-          <div class="text-center">
-            <h1 class="text-4xl font-bold tracking-tight text-gray-200 sm:text-6xl">Find the perfect book to match your mood! </h1>
-            <p class="mt-6 text-lg leading-8 text-gray-400"><span className= "text-blue-300 text-bold">From Classics to Hidden Treasures, </span>Unleash Your Reading Potential with Tailored Book Suggestions Powered by OpenAI  .</p>
-            <div class="mt-10 flex items-center justify-center gap-x-6">
-  <a href="/home" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get Reccomendations</a>
-  <!-- <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Learn more <span aria-hidden="true">→</span></a> -->
-</div>
-
-          </div>
-        </div>
-        <div class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]" aria-hidden="true">
-          <div class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" />
         </div>
       </div>
-      <div id="about">
-        <Features />
-
+      <div
+        class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+        aria-hidden="true"
+      >
+        <div
+          class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+          style="
+            clip-path: polygon(
+              74.1% 44.1%,
+              100% 61.6%,
+              97.5% 26.9%,
+              85.5% 0.1%,
+              80.7% 2%,
+              72.5% 32.5%,
+              60.2% 62.4%,
+              52.4% 68.1%,
+              47.5% 58.3%,
+              45.2% 34.5%,
+              27.5% 76.7%,
+              0.1% 64.9%,
+              17.9% 100%,
+              27.6% 76.8%,
+              76.1% 97.7%,
+              74.1% 44.1%
+            );
+          "
+        />
       </div>
-      
-      <CTA />
-      <Newsletter />
     </div>
-  </template>
-  
-  <script setup>
+    <div id="about">
+      <Features />
+    </div>
 
-  import Features from './FeaturePage.vue'
-  import CTA from './CtaPage.vue'
-  import Newsletter from './NewsLetter.vue'
+    <CTA />
+    <Newsletter />
+    <FooterPage />	
+  </div>
+</template>
 
-
-  
- 
-  </script>
+<script setup>
+import Features from "./FeaturePage.vue";
+import CTA from "./CtaPage.vue";
+import Newsletter from "./NewsLetter.vue";
+import FooterPage from "./FooterPage.vue";
+</script>
