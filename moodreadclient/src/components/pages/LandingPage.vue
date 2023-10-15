f8f65cff0358fb8168c33a8a85db1c4db028ff0a
Caroline <110534527+Ckimatu@users.noreply.github.com>
2023-10-15 15:15:58 +0000

    search bar update

--- moodreadclient/src/components/pages/LandingPage.vue
+++ moodreadclient/src/components/pages/LandingPage.vue
@@ -39,30 +39,26 @@
             Announcing our next round of funding. <a href="#" class="font-semibold text-indigo-600"><span class="absolute inset-0" aria-hidden="true" />Read more <span aria-hidden="true">&rarr;</span></a>
           </div> -->
         </div>
+        
         <div class="text-center">
           <h1 class="text-4xl font-bold tracking-tight text-gray-300 sm:text-6xl">Find the perfect book to match your mood!</h1>
-          <p class="mt-6 text-lg leading-8 text-gray-500">What are you in the mood for?
-            Don't be afraid to get creative with your descriptions - the more details you provide, the better we can match you with a book you'll love.</p>
-          <div class="mt-10 flex items-center justify-center gap-x-6">
-            <!-- <a href="#" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get started</a> -->
-            <!-- Search form -->
-          <div class="mt-10 items-center justify-center">
-            <form @submit.prevent="submitSearch">
-              <label for="default-search"
-                class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-              <div class="relative">
-                <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                  <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none"
-                    stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                      d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
-                  </svg>
-                </div>
-                <input type="search" id="default-search"
-                  class="block w-300 p-4 pl-10 text-sm text-black border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-900 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="a whimsical book filled with charming characters" v-model="searchQuery" required />
-                <button type="submit"
-                  class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+          <p class="mt-6 text-lg leading-8 text-gray-500">What are you in the mood for? Don't be afraid to get creative with your descriptions - the more details you provide, the better we can match you with a book you'll love.</p>
+          <div class="mt-10 flex flex-col items-center">
+            <form @submit.prevent="submitSearch" class="w-full max-w-3xl mx-auto">
+              <label for="default-search" class="sr-only">Search</label>
+              <div class="relative flex items-center w-full">
+                <input
+                  type="search"
+                  id="default-search"
+                  class="w-full p-4 pl-10 text-sm text-black border border-gray-300 rounded-full bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:placeholder-gray-900 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="a whimsical book filled with charming characters"
+                  v-model="searchQuery"
+                  required
+                />
+                <button
+                  type="submit"
+                  class="absolute right-2 top-2 bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 text-white font-medium rounded-full px-4 py-2 dark:bg-blue-700 dark:hover-bg-blue-800 dark:focus:ring-blue-800"
+                >
                   Search
                 </button>
               </div>
@@ -70,8 +66,8 @@
             <!-- Conditionally display the PopUp component -->
             <Pop v-if="showPopUp" />
           </div>
-          </div>
         </div>
+
       </div>
       <div class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]" aria-hidden="true">
         <div class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" />
